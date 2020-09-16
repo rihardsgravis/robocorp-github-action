f668d881ca92e74d0dc0e77be1d00041a359b302
Rihards Gravis <rihards@gravis.lv>
2020-09-17 00:15:07 +0300

    Overall update

--- index.js
+++ index.js
@@ -5,17 +5,14 @@ const fetch = require('node-fetch');
 const sleep = (seconds) => new Promise((resolve) => setTimeout(resolve, seconds * 1000));
 
 const headers = {
-  headers: {
-    'robocloud-process-secret': core.getInput('process-secret'),
-    'Content-Type': 'application/json',
-  },
+  'robocloud-process-secret': core.getInput('process-secret'),
+  'Content-Type': 'application/json',
 };
 
 async function triggerRobot() {
   const payload = core.getInput('payload');
-  console.log(payload);
   const body = payload && payload.length ? JSON.parse(payload) : {};
-  const response = await fetch(core.getInput('process-url'), { method: 'POST', body, headers });
+  const response = await fetch(core.getInput('process-url'), { method: 'POST', body: JSON.stringify({ variables: body }), headers });
   const json = await response.json();
 
   if (json.message !== 'OK') {
@@ -23,6 +20,8 @@ async function triggerRobot() {
   }
 
   const { workspaceId, processId, processRunId } = json;
+
+  console.log(`Process ${processId} triggered`);
 
   return `https://api.eu1.robocloud.eu/workspace-v1/workspaces/${workspaceId}/processes/${processId}/runs/${processRunId}`;
 }
@@ -35,21 +34,35 @@ async function waitForRobot(processUrl) {
 
   while (new Date().getTime() < endTime) {
     try {
-      const response = await fetch(processUrl, { method: 'POST', headers });
+      const response = await fetch(processUrl, { headers });
       const json = await response.json();
 
       if (json.state === 'COMPL') {
+        if (json.result === 'ERR') {
+          console.log(`Robot failed with an error`);
+          return false;
+        }
+
+        console.log('Robot completed succesfully', JSON.stringify(json));
         return true;
       }
 
-      if (json.errorCode.length) {
+      if (json.errorCode && json.errorCode.length) {
+        console.log(`Robot failed with error code ${json.errorCode}.`);
         return false;
       }
 
-      console.log(`Robot still running. Attempt ${attempt++}. ${JSON.stringify(json)}`);
+      if (json.state === 'IP') {
+        console.log(`Robot still running. Attempt ${attempt++}.`);
+      }
+
+      if (json.state === 'INI' || json.state === 'IND') {
+        console.log(`Robot initializing. Attempt ${attempt++}.`);
+      }
     } catch (e) {
-      await sleep(2);
+      console.log(e);
     }
+    await sleep(4);
   }
 
   throw new Error(`Timeout reached before the robot completed`);
